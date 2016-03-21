9e4d9260ffaa83a6160f9ea3702daa8c54c12acd
Ismail Öz <Ismail z>
2016-03-21 18:39:02 +0100

    Initial commit

---
+++ dist/main.js
@@ -0,0 +1,22 @@
+System.register(['zone.js', 'zone.js/dist/long-stack-trace-zone', 'reflect-metadata', 'angular2/platform/browser', './app.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var browser_1, app_component_1;
+    return {
+        setters:[
+            function (_1) {},
+            function (_2) {},
+            function (_3) {},
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            }],
+        execute: function() {
+            browser_1.bootstrap(app_component_1.AppComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7OztZQU9BLG1CQUFTLENBQUMsNEJBQVksQ0FBQyxDQUFDIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgJ3pvbmUuanMnO1xuaW1wb3J0ICd6b25lLmpzL2Rpc3QvbG9uZy1zdGFjay10cmFjZS16b25lJztcbmltcG9ydCAncmVmbGVjdC1tZXRhZGF0YSc7XG5cbmltcG9ydCB7Ym9vdHN0cmFwfSAgICBmcm9tICdhbmd1bGFyMi9wbGF0Zm9ybS9icm93c2VyJztcbmltcG9ydCB7QXBwQ29tcG9uZW50fSBmcm9tICcuL2FwcC5jb21wb25lbnQnO1xuXG5ib290c3RyYXAoQXBwQ29tcG9uZW50KTtcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'reflect-metadata';
+
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
